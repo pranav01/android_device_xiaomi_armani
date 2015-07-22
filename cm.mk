a8f2300370ee9ac0ecd627611caf26e3f3202550
Pranav Reddy <pranavreddy89@yahoo.in>
2015-07-22 17:36:35 +0530

    Update and rename px.mk to cm.mk

--- cm.mk
+++ cm.mk
@@ -28,11 +28,11 @@ USE_OPTIMIZATIONS := true
 USE_LEGACY_GCC := false
 
 # Inherit some common CM stuff
-$(call inherit-product, vendor/px/config/common_full_phone.mk)
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_DEVICE := armani
-PRODUCT_NAME := px_armani
+PRODUCT_NAME := cm_armani
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := HM 1S
 PRODUCT_MANUFACTURER := Xiaomi
